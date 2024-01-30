1ca174ab41193f5c139c42ca9329e115f465a18a
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-01-30 16:00:54 +0530

    Create Tours component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
+import data from "./data";
+import Tours from "./components/Tours";
 
 const App = () => {
-  return <div>App</div>;
-};
+  const[tours, setTours] = useState(data);
+  return (
+    <div>
+      
+      <Tours tours={tours}></Tours>
+    </div>
+  );
+}
 
 export default App;
