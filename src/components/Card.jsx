8b1316be3c8a4834e46ef9d30813f3f0de6b9531
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-01-30 20:19:02 +0530

    Add readmore logic

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,18 +1,26 @@
-function Card({id, name, info, image, price}){
-    const description = `${info.substring(0,200)}....`    //substring is used to cut the string from 0 to 200
+function Card({ id, name, info, image, price }) {
+    const[readmore, setReadmore]= useState(false);
+    const description = `${info.substring(0, 200)}....`; //substring is used to cut the string from 0 to 200
 
+    function readmoreHandler(){
+        setReadmore(!readmore);
+    }
 
-   return(
-    <div className="card">
+    return (
+        <div className="card">
         <img src={image} className="image"></img>
         <div className="tour-details">
             <h4 className="tour-price">{price}</h4>
             <h4 className="tour-name">{name}</h4>
         </div>
-        <div className="description">{description}</div>
-
-    </div>
-   )
+        <div className="description">
+            {description}
+            <span className="read-more" onClick={readmoreHandler}>
+                {readmore ? `show less` : `read more`}
+            </span>
+            </div>
+        </div>
+    );
 }
 
 export default Card;
