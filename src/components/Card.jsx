eeceed23d882d5aeff8de56597058563ea22618d
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-01-30 20:05:32 +0530

    Till description

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,18 @@
-function Card(){
-   
+function Card({id, name, info, image, price}){
+    const description = `${info.substring(0,200)}....`    //substring is used to cut the string from 0 to 200
+
+
+   return(
+    <div className="card">
+        <img src={image} className="image"></img>
+        <div className="tour-details">
+            <h4 className="tour-price">{price}</h4>
+            <h4 className="tour-name">{name}</h4>
+        </div>
+        <div className="description">{description}</div>
+
+    </div>
+   )
 }
 
 export default Card;
