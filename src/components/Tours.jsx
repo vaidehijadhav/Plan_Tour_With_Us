3fb0a2b2906582de0a752119e2353e6a089c81ea
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-01-30 19:53:24 +0530

    Add map function

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,7 +1,18 @@
-function Tours (){
+function Tours ({tours}){
     return(
         <div>
-            <h2>Plan With Us</h2>
+            <div>
+                <h2>Plan With Us</h2>
+            </div>
+            <div>
+                {
+                    tours.map((tour)=>{        //har ek tour ki value ke liye card return kr raha hai
+                        return <Card {...tour}></Card>   //spread operator. cloning the object
+                    })
+                }
+                
+            </div>
+            
         </div>
     );
 
