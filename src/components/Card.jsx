31682233fd5d95bc05df1a2af883986da5be3bcd
Vaidehi Jadhav <jadhav.v10122@gmail.com>
2024-01-30 22:10:43 +0530

    Add Remove Tour function

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,6 @@
-function Card({ id, name, info, image, price }) {
+import React, {useState} from "react"
+
+function Card({ id, name, info, image, price, removeTour }) {
     const[readmore, setReadmore]= useState(false);
     const description = `${info.substring(0, 200)}....`; //substring is used to cut the string from 0 to 200
 
@@ -8,17 +10,26 @@ function Card({ id, name, info, image, price }) {
 
     return (
         <div className="card">
-        <img src={image} className="image"></img>
-        <div className="tour-details">
-            <h4 className="tour-price">{price}</h4>
-            <h4 className="tour-name">{name}</h4>
-        </div>
-        <div className="description">
-            {description}
-            <span className="read-more" onClick={readmoreHandler}>
-                {readmore ? `show less` : `read more`}
-            </span>
+
+            <img src={image} className="image"></img>
+
+            <div className="tour-info">
+                <div className="tour-details">
+                    <h4 className="tour-price">{price}</h4>
+                    <h4 className="tour-name">{name}</h4>
+                </div>
+
+                <div className="description">
+                    {description}
+                    <span className="read-more" onClick={readmoreHandler}>
+                        {readmore ? `show less` : `read more`}
+                    </span>
+                </div>
             </div>
+
+            <button className="btn-red" onClick={()=> removeTour(id)}>
+                Not Interested
+            </button>
         </div>
     );
 }
